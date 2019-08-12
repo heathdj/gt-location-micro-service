e33a8f1a55310c93f3a779f047f02001339f6895
David Heath <heathdj@gmail.com>
2019-08-11 22:26:47 -0700

    Initial Project Commit.  Closes #1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gt-location-micro-service
-Location Microservice for the Geek Trek System
+# Geek Trek - Location Microservice
+This Microservice provides the information about the Locations available in the Geek Trek Rest API system.
+
+## Prerequisites
+Spring Boot
+
+### Installing
+
+TBD
+
+### More to come
---
+++ src/main/java/net/bearcreekconsulting/geektrek/location/gtlocationmicroservice/GtLocationMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package net.bearcreekconsulting.geektrek.location.gtlocationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtLocationMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtLocationMicroServiceApplication.class, args);
+    }
+
+}
